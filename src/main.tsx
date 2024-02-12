9b2203fa9716b3e81cddd70f772dcfefe2dc8c4c
강나연 <seoultownroad@naver.com>
2024-02-12 20:23:40 +0900

    feat: layout 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { RecoilRoot } from 'recoil';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Layout from './components/Layout';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +20,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <App />
+        <Layout>
+          <App />
+        </Layout>
       </RecoilRoot>
       <ReactQueryDevtools />
     </QueryClientProvider>
