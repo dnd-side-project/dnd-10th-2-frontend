beb2e5eb64acaf4fc0acbdcc469811c354109dee
강나연 <seoultownroad@naver.com>
2024-02-16 15:54:06 +0900

    feat: theme palette 색상 추가

--- src/styles/palette.tsx
+++ src/styles/palette.tsx
@@ -6,7 +6,7 @@ export const palette = {
   light_gray2: '#E7EBEF',
   light_gray2_5: '#DBE2EA',
   light_gray3: '#C7CCD6',
-  light_gray4:'#ADB3C1',
+  light_gray4: '#ADB3C1',
   middle_gray1: '#989FAE',
   middle_gray2: '#7E8799',
   middle_gray3: '#646E81',
@@ -16,7 +16,10 @@ export const palette = {
 
   main: '#353C49',
   main_blue: '#5784FD',
-  timer_liner: 'linear-gradient(to right, rgba(87, 132, 253, 1) 0%, rgba(209, 222, 255, 1) 100%)',
+  timer_trail: '#E8ECF2',
+  timer_typo: '#48556D',
+  timer_liner:
+    'linear-gradient(to right, rgba(87, 132, 253, 1) 0%, rgba(209, 222, 255, 1) 100%)',
   main_blue_dark: '#4976F0',
 
   skyblue: '#E4EAFC',
@@ -31,5 +34,5 @@ export const palette = {
   white: '#FFFFFF',
   gray: '#F7F8F9',
 
-  light_red: '#FFD7CE',
+  light_red: '#FFD7CE'
 };
