d77ef89b0c9eaa51dfa2721110271e6ed176aedb
Donghyun Lee <61397627+shubug1015@users.noreply.github.com>
2024-02-20 23:52:32 +0900

    ✨ feat : Button 컴포넌트 작성  (#19)
    * feat: Button typo 추가
    
    * feat: 카카오 로그인 버튼 색상 추가
    
    * feat: Button 컴포넌트 UI 작성
    
    * feat: Button 컴포넌트 size 타입 추가

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,65 @@
+import styled from '@emotion/styled';
+import { Size, BackgroundColor, TextColor } from './types';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  size: Size;
+  fullWidth?: boolean;
+  backgroundColor: BackgroundColor;
+  textColor?: TextColor;
+  disabled?: boolean;
+  onClick: () => void;
+}
+
+export const Button = ({
+  children,
+  size,
+  fullWidth = true,
+  backgroundColor,
+  textColor = 'white',
+  disabled = false,
+  onClick
+}: ButtonProps) => {
+  return (
+    <StyledButton
+      size={size}
+      fullWidth={fullWidth}
+      backgroundColor={backgroundColor}
+      textColor={textColor}
+      disabled={disabled}
+      onClick={onClick}>
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button<{
+  size: 'lg' | 'md' | 'sm';
+  fullWidth?: boolean;
+  backgroundColor: BackgroundColor;
+  textColor: TextColor;
+  disabled?: boolean;
+}>`
+  ${(props) =>
+    props.size === 'lg'
+      ? props.theme.typo.BL
+      : props.size === 'md'
+        ? props.theme.typo.BM1
+        : props.theme.typo.BM3}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${(props) => (props.fullWidth ? '100%' : 'auto')};
+  padding: 1.4rem 0;
+  border-radius: 1.2rem;
+  background-color: ${(props) =>
+    props.disabled
+      ? props.theme.palette.light_gray3
+      : props.theme.palette[props.backgroundColor]};
+  color: ${(props) => props.theme.palette[props.textColor]};
+  transition: opacity 0.1s ease-in-out;
+  &:hover,
+  &:focus {
+    opacity: 0.8;
+  }
+`;
