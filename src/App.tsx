b79e41776ae180874859babde13f4df5508bc932
강나연 <seoultownroad@naver.com>
2024-02-20 20:44:48 +0900

    fix: resolve conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
 import { Flex } from '@/components/Wrapper';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Timer } from '@/components/timer/Timer';
-
-function App() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  return (
-    <Flex direction="column">
-      <Timer serverTime={new Date()} time={30} />
-    </Flex>
-  );
-=======
-  return <div>파이팅~~...</div>;
->>>>>>> 0dfcdd0 (✨ feat: Input 컴포넌트 (#14))
-=======
 
 function App() {
   return <Flex direction="column">...</Flex>;
->>>>>>> 6ce395d (feat: timer 컴포넌트)
-=======
-import { Timer } from '@/components/timer/Timer';
-
-function App() {
-  return (
-    <Flex direction="column">
-      <Timer serverTime={new Date()} time={30} />
-    </Flex>
-  );
->>>>>>> f098185 (fix: timer chip position)
-=======
-  return <div>파이팅~~...</div>;
->>>>>>> origin
 }
 
 export default App;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { Flex } from '../Wrapper';
 import { ReactNode } from 'react';
+import { theme } from '@/styles';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
@@ -14,14 +15,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
 const Wrapper = styled(Flex)`
   min-height: 100vh;
   width: 375px;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
   background-color: ${theme.palette.white};
-=======
   padding: 0px 20px 0px 20px;
->>>>>>> 0dfcdd0 (✨ feat: Input 컴포넌트 (#14))
-=======
-  padding: 0px 20px 0px 20px;
->>>>>>> origin
 `;
