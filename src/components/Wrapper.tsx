d8f49c9bb0e3bf324f3e8679a1f7fe25eab8b0b2
강나연 <seoultownroad@naver.com>
2024-02-20 22:24:51 +0900

    feat: Text 컴포넌트 추가

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,3 +1,4 @@
+import { KeyOfPalette, KeyOfTypo, theme } from '@/styles';
 import styled from '@emotion/styled';
 
 export const Flex = styled.div<{
@@ -24,4 +25,17 @@ export const Space = styled.div<{
 }>`
   height: ${({ height }) => (height ? `${height}px` : '')};
   width: ${({ width }) => (width ? `${width}px` : '')};
+`;
+
+export const Text = styled.p<{
+  typo: KeyOfTypo;
+  color: KeyOfPalette;
+}>`
+  ${({ typo }) => theme.typo[typo]};
+  color: ${({ color }) => theme.palette[color]};
+  display: flex;
+  align-items: center;
+  white-space: pre-wrap;
+  margin: 0;
+  padding: 0;
 `;
