ea1c068e67b7f4d886509f1992fdfcfa574aa3d5
Donghyun Lee <shubug@naver.com>
2024-02-22 03:03:02 +0900

    chore: 유저 api 수정

--- src/apis/user/userApi.ts
+++ src/apis/user/userApi.ts
@@ -1,21 +1,25 @@
-import { BASE_URL } from '@/constants';
+import { BASE_URL, REDIRECT_URI } from '@/constants';
 import { api } from '../axois';
 
 export const userApi = {
   // 카카오 로그인
-  kakaoLogin: `${BASE_URL}/oauth2/authorization/kakao?redirect_uri=http://localhost:5173/join`,
+  kakaoLogin: `${BASE_URL}/oauth2/authorization/kakao?redirect_uri=${REDIRECT_URI}`,
   // 유저 조회
   getInfo: (token: string) =>
     api.get('/api/members', {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: token
       }
     }),
   // 닉네임 추가
-  addNickname: (token: string) =>
-    api.patch('/api/members/nickname', {
-      headers: {
-        Authorization: `Bearer ${token}`
+  addNickname: (nickname: string, token: string) =>
+    api.patch(
+      '/api/members/nickname',
+      { nickname },
+      {
+        headers: {
+          Authorization: token
+        }
       }
-    })
+    )
 };
