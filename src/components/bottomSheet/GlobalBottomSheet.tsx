9e9d436655b92dd272694587233876a4fc055b17
강나연 <seoultownroad@naver.com>
2024-02-22 17:31:23 +0900

    fix: 바텀시트 position 고정

--- src/components/bottomSheet/GlobalBottomSheet.tsx
+++ src/components/bottomSheet/GlobalBottomSheet.tsx
@@ -1,27 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
-import styled from '@emotion/styled';
 import { BottomSheet } from 'react-spring-bottom-sheet';
 import './customBottomSheet.css';
 import useBottomSheet from '@/hooks/useBottomSheet';
 
 export const GlobalBottomSheet = () => {
   const { isOpen, bottomSheet, closeGlobalSheet } = useBottomSheet();
+
   return (
     <>
-      <StyledSheet
+      <BottomSheet
+        className="bottom-sheet"
         open={isOpen}
         onDismiss={closeGlobalSheet}
-        snapPoints={({ maxHeight }) => [
-          400, //최소
-          maxHeight * 0.7 //최대
+        snapPoints={({ minHeight, maxHeight }) => [
+          minHeight + 70,
+          maxHeight - 120
         ]}>
         {bottomSheet?.content}
-      </StyledSheet>
+      </BottomSheet>
     </>
   );
 };
-
-const StyledSheet = styled(BottomSheet)`
-  width: 375px;
-`;
