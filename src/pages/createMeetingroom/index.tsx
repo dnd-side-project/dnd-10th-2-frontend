2c3ed2c1e596f5b5d294cdd978ddf92dfa2b87a7
Donghyun Lee <shubug@naver.com>
2024-03-03 23:03:15 +0900

    feat: 회의실 만들기 1차 UI 작성

--- src/pages/createMeetingroom/index.tsx
+++ src/pages/createMeetingroom/index.tsx
@@ -1,9 +1,24 @@
+import {
+  Thumbnail1,
+  Thumbnail2,
+  Thumbnail3,
+  Thumbnail4,
+  Thumbnail5,
+  Thumbnail6,
+  Thumbnail7,
+  Thumbnail8
+} from '@/assets/MeetingRoom/Thumbnail';
 import { Flex, Space } from '@/components/Wrapper';
 import { Header } from '@/components/common/Header';
+import { Input } from '@/components/common/Input';
+import { SvgIcon } from '@/components/common/SvgIcon';
 import styled from '@emotion/styled';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { KeyOfPalette } from '@/styles';
 
-const CreateMeetingroom = () => {
+const CreateMeetingRoom = () => {
+  console.log(KeyOfPalette);
   const [currentStep, setCurrentStep] = useState<number>(0);
   const stepList = [
     {
@@ -19,6 +34,21 @@ const CreateMeetingroom = () => {
       name: '회의 장소'
     }
   ];
+
+  const {
+    register,
+    watch,
+    getValues,
+    // setValue,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      meetingRoomName: '',
+      meetingRoomNotice: ''
+    },
+    mode: 'onChange'
+  });
+  console.log(getValues('meetingRoomName'));
   return (
     <Flex direction="column" align="flex-start">
       <Header title="회의 만들기" iconLeftId="arrow_left" />
@@ -37,6 +67,64 @@ const CreateMeetingroom = () => {
           </StyledStep>
         ))}
       </StyledStepList>
+
+      <Space height={30} />
+
+      <StyledLabel>
+        회의 이름을 알려주세요 <SvgIcon id="star_orange" size={18} />
+      </StyledLabel>
+      <Input
+        {...register('meetingRoomName', {
+          required: '회의 이름을 입력해주세요',
+          maxLength: {
+            message: '최대 15자까지 입력가능해요',
+            value: 15
+          }
+        })}
+        value={watch('meetingRoomName')}
+        type="default"
+        placeholder="회의 이름"
+        maxLength={15}
+        isError={errors.meetingRoomName ? true : false}
+        errorText={errors.meetingRoomName?.message as string}
+      />
+
+      <Space height={4} />
+
+      <StyledLabel>회의 공지가 있다면 적어주세요</StyledLabel>
+      <Input
+        {...register('meetingRoomNotice', {
+          maxLength: {
+            message: '최대 15자까지 입력가능해요',
+            value: 30
+          }
+        })}
+        multiline={true}
+        value={watch('meetingRoomNotice')}
+        type="default"
+        placeholder="회의 이름"
+        maxLength={30}
+        isError={errors.meetingRoomNotice ? true : false}
+        errorText={errors.meetingRoomNotice?.message as string}
+        height={92}
+      />
+
+      <Space height={4} />
+
+      <StyledLabel>
+        썸네일을 골라주세요 <SvgIcon id="star_orange" size={18} />
+      </StyledLabel>
+
+      <StyledThumbnailList>
+        <Thumbnail1 />
+        <Thumbnail2 />
+        <Thumbnail3 />
+        <Thumbnail4 />
+        <Thumbnail5 />
+        <Thumbnail6 />
+        <Thumbnail7 />
+        <Thumbnail8 />
+      </StyledThumbnailList>
     </Flex>
   );
 };
@@ -78,4 +166,20 @@ const StyledSteName = styled.div<{ isCurrentStep: boolean }>`
       : props.theme.palette.light_gray4};
 `;
 
-export default CreateMeetingroom;
+const StyledLabel = styled.label`
+  ${(props) => props.theme.typo.T5}
+  color: ${(props) => props.theme.palette.dark_gray2};
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+  margin-bottom: 1rem;
+`;
+
+const StyledThumbnailList = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+  gap: 1rem;
+`;
+
+export default CreateMeetingRoom;
