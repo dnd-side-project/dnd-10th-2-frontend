15d35cce5a838d6d1fb6b6c151db883755895a8f
Donghyun Lee <shubug@naver.com>
2024-03-07 00:20:19 +0900

    feat: thumbnail list 컴포넌트 생성

---
+++ src/assets/MeetingRoom/thumbnailList.ts
@@ -0,0 +1,45 @@
+import {
+  Thumbnail1,
+  Thumbnail2,
+  Thumbnail3,
+  Thumbnail4,
+  Thumbnail5,
+  Thumbnail6,
+  Thumbnail7,
+  Thumbnail8
+} from './Thumbnail';
+
+export const thumbnailList = [
+  {
+    id: 0,
+    icon: Thumbnail1
+  },
+  {
+    id: 1,
+    icon: Thumbnail2
+  },
+  {
+    id: 2,
+    icon: Thumbnail3
+  },
+  {
+    id: 3,
+    icon: Thumbnail4
+  },
+  {
+    id: 4,
+    icon: Thumbnail5
+  },
+  {
+    id: 5,
+    icon: Thumbnail6
+  },
+  {
+    id: 6,
+    icon: Thumbnail7
+  },
+  {
+    id: 7,
+    icon: Thumbnail8
+  }
+];
