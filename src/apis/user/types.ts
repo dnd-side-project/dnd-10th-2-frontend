8123ef648249a0e8864da3b62021ae619b094eab
Donghyun Lee <shubug@naver.com>
2024-06-17 20:02:50 +0900

    feat: 유저 api의 types 작성

---
+++ src/apis/user/types.ts
@@ -0,0 +1,6 @@
+export interface GetUserResponse {
+  response: {
+    id: number;
+    nickname: string;
+  };
+}
--- src/apis/user/userApi.ts
+++ src/apis/user/userApi.ts
@@ -1,16 +1,22 @@
 import { BASE_URL, REDIRECT_URI } from '@/constants';
 import { api } from '../axois';
+import { GetUserResponse } from './types';
 
 export const userApi = {
   // 카카오 로그인
   GET_KAKAO_LOGIN: `${BASE_URL}/oauth2/authorization/kakao?redirect_uri=${REDIRECT_URI}`,
   // 유저 조회
-  GET_MEMBERS: (token: string) =>
-    api.get('/api/members', {
+  GET_MEMBERS: async (token: string) => {
+    const {
+      data: { response }
+    } = await api.get<GetUserResponse>('/api/members', {
       headers: {
         Authorization: token
       }
-    }),
+    });
+
+    return response;
+  },
   // 닉네임 추가
   PATCH_NICKNAME: (nickname: string, token: string) =>
     api.patch(
