86cf94ff762af52bb7fc00db8faa6614e993809f
Donghyun Lee <shubug@naver.com>
2024-06-20 20:49:26 +0900

    refactor: 회의 대한 shared 계층 생성

---
+++ src/shared/meeting/apis/mutations/useCreateMeeting.ts
@@ -0,0 +1,53 @@
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+
+import { api } from '@shared/common/api';
+import type {
+  CreateMeetingRequest,
+  CreateMeetingResponse
+} from '@shared/meeting/apis/types';
+
+export const useCreateMeeting = ({
+  token,
+  title,
+  description,
+  imageNum,
+  startTime,
+  estimatedTotalDuration,
+  location
+}: CreateMeetingRequest) => {
+  const navigate = useNavigate();
+
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      const {
+        data: { response }
+      } = await api.post<CreateMeetingResponse>(
+        '/api/meetings',
+        {
+          title,
+          location,
+          startTime,
+          description,
+          estimatedTotalDuration,
+          imageNum
+        },
+        {
+          headers: {
+            Authorization: token
+          }
+        }
+      );
+
+      return response;
+    },
+    onError: () => {
+      console.log('error');
+    },
+    onSuccess: ({ meetingId }) => {
+      navigate(`/meeting-room/${meetingId}`);
+    }
+  });
+
+  return { mutate };
+};
