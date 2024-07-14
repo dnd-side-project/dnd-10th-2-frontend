2d6cd3b705a3f03764794353ad6a6528b99c39f5
Donghyun Lee <shubug@naver.com>
2024-07-14 17:12:01 +0900

    feat: api req, res 타입 작성

--- src/shared/meeting/apis/index.ts
+++ src/shared/meeting/apis/index.ts
@@ -1,1 +1,5 @@
 export * from './mutations/useCreateMeeting';
+export * from './mutations/useAddAgenda';
+export * from './queries/useGetMeeting';
+export * from './queries/useGetMeetingMemberList';
+export * from './queries/useGetAgendaList';
--- src/shared/meeting/apis/types.ts
+++ src/shared/meeting/apis/types.ts
@@ -15,3 +15,82 @@ export interface CreateMeetingResponse {
     // shareUrl: 'generated_url';}
   };
 }
+
+export interface GetMeetingRequest {
+  token: string | null;
+  meetingId: string;
+}
+
+export interface GetMeetingResponse {
+  response: {
+    meetingId: number;
+    title: string;
+    description: string;
+    meetingStatus: string;
+    hostMemberId: number;
+    startTime: string;
+    totalEstimatedDuration: string; // 회의 예상 소요시간
+    currentDuration: string; // 회의 실제 소요시간
+    imgNum: number;
+  };
+}
+
+export interface GetMeetingMemberListRequest {
+  token: string | null;
+  meetingId: string;
+}
+
+export interface MeetingMemberResponse {
+  memberId: string;
+  nickname: string;
+  imageNum: number;
+}
+
+export interface GetMeetingMemberListResponse {
+  response: {
+    hostMember: {
+      memberId: string;
+      nickname: string;
+      imageNum: number;
+    };
+    members: MeetingMemberResponse[];
+    host: boolean; // 방장 여부
+  };
+}
+
+export interface GetAgendaListRequest {
+  token: string | null;
+  meetingId: string;
+}
+
+export interface AgendaResponse {
+  agendaId: number;
+  title: string;
+  type: 'AGENDA' | 'BREAK';
+  currentDuration: string;
+  remainingDuration: string;
+  status: 'PENDING' | 'INPROGRESS' | 'PAUSED' | 'COMPLETED';
+}
+
+export interface GetAgendaListResponse {
+  response: {
+    meetingId: number;
+    currentDuration: string; // 현재까지 회의 소요시간
+    agendaResponse: AgendaResponse[];
+  };
+}
+
+export interface AddAgendaRequest {
+  token: string | null;
+  meetingId: string;
+  title: string;
+  type: 'AGENDA' | 'BREAK'; // 안건이면 AGENDA, 쉬는시간이면 BREAK
+  duration: string;
+  onSuccess: () => void;
+}
+
+export interface AddAgendaResponse {
+  response: {
+    agendaId: number;
+  };
+}
