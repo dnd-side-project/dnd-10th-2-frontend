b896e2a256bbabe42c5e52d76e9f2f6109a2e513
Donghyun Lee <shubug@naver.com>
2024-08-29 23:13:57 +0900

    feat: 웹소켓 코드 export

--- src/shared/meeting/apis/index.ts
+++ src/shared/meeting/apis/index.ts
@@ -3,7 +3,11 @@ export * from './mutations/useAddAgenda';
 export * from './mutations/useDeleteAgenda';
 export * from './mutations/useEditAgenda';
 export * from './mutations/useReorderAgendaList';
+export * from './mutations/useEndMeeting';
 
 export * from './queries/useGetMeeting';
 export * from './queries/useGetMeetingMemberList';
 export * from './queries/useGetAgendaList';
+
+export * from './webSockets/useGetMeetingDuration';
+export * from './webSockets/useControlAgenda';
