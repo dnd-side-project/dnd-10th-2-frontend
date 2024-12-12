aa04031b2509f6265893ea48da53a21b8eb43781
Donghyun Lee <61397627+shubug1015@users.noreply.github.com>
2024-12-12 21:57:42 +0900

    ✨ feat: 나의 회의실 페이지 UI 작성 (#86)
    * feat: 헤더, 버튼 작성
    
    * feat: 모든 회의 조회 api 작성
    
    * feat: 폴더 아이콘 추가

---
+++ src/features/main/assets/FolderIcon.tsx
@@ -0,0 +1,65 @@
+export const FolderIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="145"
+      height="122"
+      viewBox="0 0 145 122"
+      fill="none">
+      <g filter="url(#filter0_d_1014_18973)">
+        <rect
+          x="16.5547"
+          y="22.3223"
+          width="105.268"
+          height="47.3239"
+          rx="6"
+          transform="rotate(-8.91976 16.5547 22.3223)"
+          fill="white"
+        />
+      </g>
+      <path
+        d="M47.5845 30.0558L55.1756 38.7579C56.315 40.0641 57.9637 40.8138 59.697 40.8138H129C133.418 40.8138 137 44.3955 137 48.8138V109C137 113.418 133.418 117 129 117H16.0001C11.5819 117 8.00014 113.418 8.00013 109L8.00001 36C8.00001 31.5817 11.5817 28 16 28H43.0631C44.7964 28 46.4451 28.7496 47.5845 30.0558Z"
+        fill="#E7EBEF"
+      />
+      <circle cx="56.8028" cy="76.7227" r="3.80282" fill="white" />
+      <circle cx="72.8595" cy="76.7227" r="3.80282" fill="white" />
+      <circle cx="88.9161" cy="76.7227" r="3.80282" fill="white" />
+      <defs>
+        <filter
+          id="filter0_d_1014_18973"
+          x="10.0116"
+          y="1.45742"
+          width="124.419"
+          height="76.1594"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2" />
+          <feGaussianBlur stdDeviation="3.7" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.567427 0 0 0 0 0.567427 0 0 0 0 0.567427 0 0 0 0.15 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_1014_18973"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_1014_18973"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
