1580ea52e6445062e665d8cf50db920500a3c7a0
Donghyun Lee <61397627+shubug1015@users.noreply.github.com>
2025-02-04 22:44:28 +0900

    🐛 fix: Timer 컴포넌트의 남은 시간 에러 수정 (#105)
    * feat: Timer 컴포넌트 남은 시간 에러 수정
    
    * feat: api base url 변경

--- src/shared/common/constants/index.ts
+++ src/shared/common/constants/index.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://facerain-dev.iptime.org:8080';
+export const BASE_URL = 'https://facerain-dev.duckdns.org';
 export const BROKER_URL = 'ws://facerain-dev.iptime.org:8080/ws';
